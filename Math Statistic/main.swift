04edf548665ba2d0e37f49f2e96afe7e6d71d740
Akbar <aumetov7@gmail.com>
2020-06-03 17:14:58 +0300

    Initial Commit

---
+++ Math Statistic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Math Statistic
+//
+//  Created by Акбар Уметов on 03.06.2020.
+//  Copyright © 2020 aumetov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
